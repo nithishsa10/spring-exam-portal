c2f5d5f054c630d3b4b8bab2f448446e623e1596
nithishsa10 <nithishsav5@gmail.com>
2024-09-18 16:21:50 +0530

    repository

---
+++ src/main/java/com/exam/portal/repository/CategoryRepository.java
@@ -0,0 +1,4 @@
+package com.exam.portal.repository;
+
+public class CategoryRepository {
+}
---
+++ src/main/java/com/exam/portal/repository/ResultRepository.java
@@ -0,0 +1,6 @@
+package com.exam.portal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultRepository extends JpaRepository {
+}
